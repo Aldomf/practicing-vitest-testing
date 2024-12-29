6d5c312a65525dc879ab8dce02805abd4db4ad43
Aldo Miralles <alditomiralles3@gmail.com>
2024-12-28 23:10:31 -0500

    adding video

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         </a>
         <p>opih</p>
         <h2>sss</h2>
+        <video src="https://www.w3schools.com/html/mov_bbb.mp4" controls></video>
       </div>
       <p>errty</p>
       <h1>aa</h1>
