7bc0ac10aa695b4cbd5ca9f621c4ee1d99e8ec33
Aldo Miralles <alditomiralles3@gmail.com>
2025-02-08 17:24:21 -0500

    deleting video tag

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <video src="https://www.w3schools.com/html/mov_bbb.mp4" controls></video>
         <img src="https://www.w3schools.com/html/pic_trulli.jpg" alt="picture of a truck" width="150" height="200" />
       </div>
       <p>errty</p>
